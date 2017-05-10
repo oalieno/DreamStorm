531de4166aa93510a920abf128c750aac9115fb8
OAlienO <jeffrey6910@gmail.com>
2017-05-10 14:57:12 +0800

    minor update

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ setup(
     name = 'DreamStorm',
     packages = ['DreamStorm'], # this must be the same as the name above
     version = '0.1',
-    description = 'A random test lib',
-    author = 'Peter Downs',
-    author_email = 'peterldowns@gmail.com',
-    url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+    description = 'multi-thread tor crawler library',
+    author = 'OAlienO',
+    author_email = 'jeffrey6910@gmail.com',
+    url = 'https://github.com/OAlienO/DreamStorm', # use the URL to the github repo
     download_url = 'https://github.com/OAlienO/DreamStorm/archive/0.1.tar.gz', # I'll explain this in a second
     keywords = ['crawler', 'tor', 'multi-thread'], # arbitrary keywords
     classifiers = [],
