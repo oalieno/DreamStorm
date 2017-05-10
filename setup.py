87a9d9a0d87ed271d355e94c5c4d470388e94fb6
OAlienO <jeffrey6910@gmail.com>
2017-05-10 18:23:00 +0800

    update to 0.2

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
     name = 'DreamStorm',
-    packages = ['DreamStorm'], # this must be the same as the name above
-    version = '0.1',
+    packages = ['DreamStorm','DreamStorm.lib'], # this must be the same as the name above
+    version = '0.2',
     description = 'multi-thread tor crawler library',
     author = 'OAlienO',
     author_email = 'jeffrey6910@gmail.com',
     url = 'https://github.com/OAlienO/DreamStorm', # use the URL to the github repo
-    download_url = 'https://github.com/OAlienO/DreamStorm/archive/0.1.tar.gz', # I'll explain this in a second
+    download_url = 'https://github.com/OAlienO/DreamStorm/archive/0.2.tar.gz', # I'll explain this in a second
     keywords = ['crawler', 'tor', 'multi-thread'], # arbitrary keywords
     classifiers = [],
 )
