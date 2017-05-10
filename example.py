63de4e23f7de0ef770748ca88113e498ee729f3d
OAlienO <jeffrey6910@gmail.com>
2017-05-11 02:39:24 +0800

    using yapf python code auto-formatter again

--- example.py
+++ example.py
@@ -1,8 +1,10 @@
 from DreamStorm import DreamStorm
 
-def example_callback(page,headers):
+
+def example_callback(page, headers):
     print "server : " + headers["server"]
 
-dream = DreamStorm(5,tor = True)
-dream.put(["https://oalieno.github.io","http://www.nctu.edu.tw"])
+
+dream = DreamStorm(5, tor=True)
+dream.put(["https://oalieno.github.io", "http://www.nctu.edu.tw"])
 dream.run(example_callback)
