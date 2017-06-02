51ade28ff40b6d967c956ecf8def9f1afaa497f4
OAlienO <jeffrey6910@gmail.com>
2017-06-02 23:53:26 +0800

    update new version

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ setup(
     packages=['DreamStorm','DreamStorm.lib','DreamStorm.lib.modules'],  # this must be the same as the name above
     install_requires=[
         'beautifulsoup4',
+        'lxml'
     ],
-    version='0.4',
+    version='0.4.1',
     description='multi-thread tor crawler library',
     author='OAlienO',
     author_email='jeffrey6910@gmail.com',
     url='https://github.com/OAlienO/DreamStorm',  # use the URL to the github repo
-    download_url='https://github.com/OAlienO/DreamStorm/archive/0.4.tar.gz',  # I'll explain this in a second
+    download_url='https://github.com/OAlienO/DreamStorm/archive/0.4.1.tar.gz',  # I'll explain this in a second
     keywords=['crawler', 'tor', 'multi-thread'],  # arbitrary keywords
     classifiers=[], )
